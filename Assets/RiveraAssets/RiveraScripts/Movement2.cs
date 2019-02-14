3ad7ad52043bce58a41b4a2f3638f2899ea21e11
armondrivera <47197165+armondrivera@users.noreply.github.com>
2019-02-14 14:20:38 -0500

    Dash controls added
    dash has been perfected in game.

--- Assets/RiveraAssets/RiveraScripts/Movement2.cs
+++ Assets/RiveraAssets/RiveraScripts/Movement2.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Movement2 : MonoBehaviour
 {
-    //The code for health subtracting may need to be a separate script
-    //Check this link for a solution
-    //https://answers.unity.com/questions/369380/lose-health-on-collision.html
-
     public Rigidbody playerRb;
     public Transform playerT;
     public float moveSpeed = 0.14f;
-    private float dashSpeed = 16f;
+    public float dashSpeed = 10.0f;
+
+    private bool isDashing = false;
+
     public float upForce = 5f;
     public float bullUpForce = 5f;
+
     public float shotTimer = 1.5f;
+    public float dashTimer = 0.5f;
+
     public float rotatePos = 0.0f;
+
     public int jumpShotCount = 0;
+
     public Animator playerA;
+
     public int plat = 0;
     //private float temp = 0.14f;
 
@@ -48,28 +53,44 @@ public class Movement2 : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            Dash();
+            isDashing = true;
+        }
+
+        if (isDashing == true)
+        {
+            dashTimer -= Time.deltaTime;
+            if (dashTimer > 0f)
+            {
+                Dash();
+            }
+
+            if (dashTimer <= 0f)
+            {
+                dashTimer = 0.5f;
+                transform.position += Vector3.zero;
+                playerRb.velocity = Vector3.zero;
+                isDashing = false;
+            }
         }
 
         //Move Left
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && isDashing == false)
         {
-            playerA.SetBool("move", true);
-            playerA.SetBool("grounded", true);
+            //playerA.SetBool("move", true);
+            //playerA.SetBool("grounded", true);
             MoveLeft();
         }
         
         //MoveRight
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && isDashing == false)
         {
-            playerA.SetBool("move", true);
-            playerA.SetBool("grounded", true);
+            //playerA.SetBool("move", true);
+            //playerA.SetBool("grounded", true);
             MoveRight();
         }
-        
 
         //Rotate
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && isDashing == false)
         {
             if (rotatePos != 180)
             {
@@ -80,7 +101,7 @@ public class Movement2 : MonoBehaviour
         
 
         //Rotate
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && isDashing == false)
         {
             if (rotatePos != 0)
             {
@@ -128,7 +149,6 @@ public class Movement2 : MonoBehaviour
     void MoveDown()
     {
         Physics.gravity = new Vector3(0, -1.0F, 0);
-        //playerRb.velocity.y = playerRb.velocity.y * 0.5f;
     }
 
     void MoveLeft()
@@ -144,7 +164,18 @@ public class Movement2 : MonoBehaviour
 
     void Dash()
     {
-        playerRb.AddForce(Vector3.right * dashSpeed);
+        if (rotatePos == 0)
+        {
+            gameObject.transform.position += new Vector3(1, 0, 0) * 0.6f;
+            //playerRb.AddForce(Vector3.right * dashSpeed);
+        }
+
+        if (rotatePos == 180)
+        {
+            gameObject.transform.position += new Vector3(-1, 0, 0) * 0.6f;
+            //playerRb.AddForce(-Vector3.right * dashSpeed);
+        }
+
     }
 
     void FlipLeft()
