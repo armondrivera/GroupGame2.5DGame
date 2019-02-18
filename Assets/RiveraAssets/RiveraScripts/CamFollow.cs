aa2c61dd98e06bf4d24fc372a6c41e243b4f2c1f
armondrivera <47197165+armondrivera@users.noreply.github.com>
2019-02-18 17:34:58 -0500

    Elec Trap created
    need a little more code ran out of time in class

---
+++ Assets/RiveraAssets/RiveraScripts/CamFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float followSpeed;
+
+    //distance between ship and camera
+    private float camToPlayerDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        camToPlayerDistance = Vector2.Distance(transform.position, player.transform.position);
+
+        //goes from one position to another position by a certain amount
+        transform.position = Vector2.Lerp(transform.position, player.transform.position, camToPlayerDistance * followSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/RiveraAssets/RiveraScripts/ElecTrap.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElecTrap : MonoBehaviour
+{
+
+    private float trapTimer = 4;
+    private float activeTimer = 2;
+    private bool trapActive = false;
+    public GameObject player;
+    private int damage = 2;
+
+    public float sight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray trapRay = new Ray(transform.position, Vector3.down);
+
+        if (trapTimer > 0)
+        {
+            trapTimer -= Time.deltaTime;
+        }
+
+        if (trapTimer <= 0)
+        {
+            trapActive = true;
+        }
+
+        if (trapActive == true)
+        {
+            activeTimer -= Time.deltaTime;
+        }
+
+        if (activeTimer <= 0)
+        {
+            trapActive = false;
+            trapTimer = 4;
+            activeTimer = 2;
+        }
+
+        if (trapActive == true && Physics.Raycast(trapRay, out hit, sight))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                player.gameObject.GetComponent<HealthScript>().TakeDamage(damage);
+            }
+        }
+        
+    }
+}
