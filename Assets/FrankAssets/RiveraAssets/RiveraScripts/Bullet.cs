4d47209ca2104015baf135e94ae966d84993fda9
armondrivera <47197165+armondrivera@users.noreply.github.com>
2019-03-02 22:58:51 -0500

    Revert "aeiou"
    This reverts commit df418b070aa9223591e63be41e4b556baac3a05c.

---
+++ Assets/FrankAssets/RiveraAssets/RiveraScripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "Sight")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/FrankAssets/RiveraAssets/RiveraScripts/HealthScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//Let's assume (please) that the HP is integers only? C: (You can adapt this code and also work with Image sliders later! yw.)
+public class HealthScript : MonoBehaviour {
+	[SerializeField] private int maxHP = 10;
+	[SerializeField] private bool alive = true;
+	private int curHP;
+
+	public event UnityAction onHPChanged;
+
+	public int HP {
+		get { return curHP; }
+		set {
+			curHP = Mathf.Clamp(value, 0, maxHP);
+
+			Debug.Log("The player's HP is now " + curHP + "!");
+			if (curHP <= 0) {
+				alive = false;
+				GameObject.Destroy(gameObject);
+			}
+
+			if (onHPChanged != null)
+				onHPChanged();
+		}
+	}
+
+	public int MaxHP {
+		get { return maxHP; }
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+		HP = maxHP;
+	}
+
+	// Update is called once per frame
+	public void Update() {
+		if (Input.GetKeyDown(KeyCode.H)) {
+			HP--;
+		}
+	}
+
+	public void TakeDamage(int amount) {
+		if (!alive)
+			return;
+		HP -= amount;
+	}
+}
