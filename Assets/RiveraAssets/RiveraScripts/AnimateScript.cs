6c9fa8aef5c842fd84c5cee99e8695f42b757cbb
armondrivera <47197165+armondrivera@users.noreply.github.com>
2019-03-02 23:06:34 -0500

    Revert "Revert "fixing""
    This reverts commit 9312da4d67bc59c5544985f2f527bba8bb626d3a.

--- Assets/RiveraAssets/RiveraScripts/AnimateScript.cs
+++ Assets/RiveraAssets/RiveraScripts/AnimateScript.cs
@@ -4,135 +4,7 @@ using UnityEngine;
 
 public class AnimateScript : MonoBehaviour
 {
-    [Tooltip("The minimum speed in which the player must be moving in m/s, such that if they stopped or reverse direction, they will have to turn.")]
-    [Range(0, 30)]
-    [SerializeField] private float turnSpeedThreshold = 10;
-
-    private Animator animator;
-    private float dashTimer = 0.8f;
-    private float jumpTwo = 0.8f;
-
-    //Velocity is a vector -- means it has multiple components or "dimensions" -- made of SEVERAL numbers.
-    private Vector3 prevVelocity;
-    private float prevSignedSpeed;
-    private Vector3 prevPosition;
-    private Vector3 position;
-    //Note that speed is a scalar AND is NEVER negative.
-
-    public void Awake()
-    {
-        animator = GetComponent<Animator>();
-
-        prevPosition = transform.position;
-        position = prevPosition;
-    }
-
-    public void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow) && animator.GetBool("GroundedId") && !animator.GetBool("DashId") && dashTimer != 0.8 || Input.GetKey(KeyCode.RightArrow)
-            && animator.GetBool("GroundedId") && !animator.GetBool("DashId"))
-        {
-            animator.SetBool("RunId", true);
-        }
-        else
-        {
-            animator.SetBool("RunId", false);
-        }
-
-        /*if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) 
-            && playerAnim.GetBool(GroundedId) == true && playerAnim.GetBool(DashId) == false)
-        {
-            playerAnim.SetBool(RunId, false);
-        }*/
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            animator.SetBool("RunId", false);
-            animator.SetBool("DashId", true);
-            animator.SetBool("RunId", false);
-        }
-
-        if (animator.GetBool("DashId") == true)
-        {
-            dashTimer -= Time.fixedDeltaTime;
-        }
-
-        if (dashTimer <= 0)
-        {
-            animator.SetBool("DashId", false);
-            dashTimer = 0.8f;
-            animator.SetBool("JumpId", false);
-            animator.SetBool("SecondJumpId", false);
-        }
-
-        if (gameObject.GetComponent<Movement2>().plat == 0)
-        {
-            animator.SetBool("JumpId", true);
-            animator.SetBool("GroundedId", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z) && animator.GetBool("JumpId") == true)
-        {
-            animator.SetBool("SecondJumpId", true);
-            jumpTwo -= Time.fixedDeltaTime;
-            animator.SetBool("GroundedId", false);
-        }
-
-        if (jumpTwo <= 0)
-        {
-            animator.SetBool("SecondJumpId", false);
-            jumpTwo = 0.8f;
-        }
-
-        if (gameObject.GetComponent<Movement2>().plat == 1)
-        {
-            animator.SetBool("JumpId", false);
-            animator.SetBool("HitGroundId", true);
-            animator.SetBool("GroundedId", true);
-            animator.SetBool("SecondJumpId", false);
-            jumpTwo = 0.8f;
-        }
-
-        TurnUpdate();
-    }
-
-    private void TurnUpdate()
-    {
-        position = transform.position;
-
-        //Whenever you have deltas, you always use "final - initial". Ex: Δx = (x_f - x_i)
-        Vector3 velocity = (position - prevPosition) / Time.fixedDeltaTime; //v = Δx / Δt		or	v = dx/dt
-        float signedSpeed = velocity.magnitude;
-
-        if (Mathf.Abs(prevSignedSpeed) >= turnSpeedThreshold)
-        {
-            //Positive previous signed speed
-            if (prevSignedSpeed > 0)
-            {
-                if (signedSpeed <= 0)
-                    animator.SetTrigger("TurnId");
-            }
-            else
-            { //Negative previous signed speed
-                if (signedSpeed >= 0)
-                    animator.SetTrigger("TurnId");
-            }
-        }
-
-        prevVelocity = velocity;
-        prevSignedSpeed = signedSpeed;
-        prevPosition = position;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Floor")
-        {
-            animator.SetBool("GroundedId", true);
-            jumpTwo = 0.8f;
-        }
-    }
-    /*Animator playerAnim;
+    Animator playerAnim;
     private float dashTimer = 0.8f;
     private float jumpTwo = 0.8f;
     // Start is called before the first frame update
@@ -154,11 +26,11 @@ public class AnimateScript : MonoBehaviour
             playerAnim.SetBool("Run", false);
         }
 
-        if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) 
+        /*if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) 
             && playerAnim.GetBool("Grounded") == true && playerAnim.GetBool("Dash") == false)
         {
             playerAnim.SetBool("Run", false);
-        }
+        }*/
 
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -216,5 +88,5 @@ public class AnimateScript : MonoBehaviour
             playerAnim.SetBool("Grounded", true);
             jumpTwo = 0.8f;
         }
-    }*/
+    }
 }
